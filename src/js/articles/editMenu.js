2dc291fea6560dc09e0cb2fe6f4dddb2c13ddd5b
hikeislife <hikeislife@users.noreply.github.com>
2020-02-05 23:16:24 +0100

    article editing page context menu basic wiring

--- src/js/articles/editMenu.js
+++ src/js/articles/editMenu.js
@@ -1,10 +1,52 @@
-const editor = document.querySelector('.editContent')
-editor.addEventListener('contextmenu', (e) => {
-  e.preventDefault()
+showHideMenu = (() => {
+  const editor = document.querySelector('.editContent')
+  const empty = document.querySelector('body')
   const menu = document.querySelector('.editMenu')
-  menu.style.display = "inline-block"
-  menu.style.top = `${e.layerY - 10}px`
-  menu.style.left = `${e.layerX}px`
-  console.log(e)
-})
+  let cursorPos, element
+
+  editor.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+    menu.style.display = "inline-block"
+    menu.style.top = `${e.layerY - 10}px`
+    menu.style.left = `${e.layerX}px`
+    cursorPos = document.getSelection().anchorOffset
+    //element = document.getSelection().anchorNode.ownerDocument.activeElement
+    element = e.originalTarget
+    //console.log(e)
+  })
+
+  empty.addEventListener('click', (e) => {
+    menu.style.display = "none"
+  })
+
+  menu.querySelectorAll('ul li').forEach(li => {
+    li.addEventListener('click', e => {
+      insertSnippet(e.target.dataset.insert, cursorPos, element)
+    })
+  })
+})()
+
+insertSnippet = (snippet, cursorPos, element) => {
+  const editor = document.querySelector('.editContent')
+  //let strPos = editor.selectionStart
+  let insertSnippet
+  switch(snippet) {
+    case "superbox":
+      insertSnippet = `<div class="superbox sol">EDIT</div>`
+      break
+    default:
+      insertSnippet = `\n`
+  }
+  console.log(insertSnippet)
+  const strBefore = element.innerText.substring(0, cursorPos)
+  const strAfter = element.innerText.substring(cursorPos, element.innerText.length)
+  element.innerHTML = `${strBefore}${insertSnippet}${strAfter}`
+}
+
+
+
+
+
+
+
 
