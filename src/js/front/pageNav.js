b590a5dd35a3884fbafa4a782b6319fb9dddcf1e
hikeislife <hikeislife@users.noreply.github.com>
2020-02-06 19:53:50 +0100

    some files moved, page navigation added

--- src/js/front/pageNav.js
+++ src/js/front/pageNav.js
@@ -1,7 +1,6 @@
 /*
  * colects all page titles and adds bookmarks to them at the start of the page
  */
-
 export default function createPageNav () {
   const page = document.querySelector('.placeholder')
   const bookmarks = [...page.querySelectorAll('a[name]')]
@@ -10,10 +9,13 @@ export default function createPageNav () {
   bookmarks.forEach(x => {
     navContent += `
       <a href="#${x.attributes.name.nodeValue}" 
-      aria-label="${x.attributes["aria-label"].nodeValue}" 
-      rel="bookmark subsection" hreflang="sr">
-      <li>${x.attributes.title.nodeValue}</li></a>\n`
+         aria-label="${x.attributes["aria-label"].nodeValue}" 
+         rel="bookmark subsection" 
+         hreflang="sr">
+        <li>${x.attributes.title.nodeValue}</li>
+      </a>\n`
   })
-  page.querySelector('.page-nav > .listless').innerHTML = navContent
-  //console.log(navContent)
+  console.log(navContent)
+  if (page.querySelector('.page-nav > .listless'))
+    page.querySelector('.page-nav > .listless').innerHTML = navContent
 }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import bindMenu from "./js/front/bindMenu.js";
 import mobileSideNavHandler from './js/front/mobileSideNav.js'
 import copyd from "./js/front/copyDate.js";
-import pageNav from "./js/front/pageNav.js";
+import createPageNav from "./js/front/pageNav.js";
 import referenceList from "./js/front/renderReferenceList.js"
 //import renderSideNav from "./js/front/renderSideNav.js";
 
 
 window.onscroll = () => bindMenu()
 //renderSideNav()
-
+createPageNav()
 // const setCourse = (() => {
 //   const url = document.URL.split('/')
 //   if (url.length <= 4) return renderSideNav('mp')
