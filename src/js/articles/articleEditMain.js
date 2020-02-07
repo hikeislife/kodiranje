3779189212642721822847caa77b9f8f002d13e3
hikeislife <hikeislife@users.noreply.github.com>
2020-02-07 04:46:17 +0100

    fixed select field on edit article page

--- src/js/articles/articleEditMain.js
+++ src/js/articles/articleEditMain.js
@@ -1,4 +1,5 @@
 import guiCodeSwitcher             from "./guiCodeSwitcher.js"
 import * as showHideMenu           from "./editMenu.js"
+import fixSelected                 from "./fixSelected.js"
 
 console.log('article edit main')
---
+++ src/js/articles/fixSelected.js
@@ -0,0 +1,8 @@
+export default (function fixSelected() {
+  const selectField = document.querySelector('select#courseName')
+  const selectedOption = selectField.dataset.selected
+  selectField.querySelectorAll('option').forEach(x => {
+    if(x.value === selectedOption) x['selected'] = 'selected'
+  })
+  //console.log(selectField.querySelectorAll('option'))
+})()
