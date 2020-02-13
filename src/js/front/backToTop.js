63c649291d56885293aaeb6829ee0f57c01951e0
hikeislife <hikeislife@users.noreply.github.com>
2020-02-13 21:33:40 +0100

    positioning of back to top button

---
+++ src/js/front/backToTop.js
@@ -0,0 +1,22 @@
+const showBackToTop = e => {
+  const backToTop = document.querySelector('.backToTop')
+  const firstAppearance = document.documentElement.scrollTop
+  const maxAppearance = document.documentElement.scrollTopMax
+  if (document.body.scrollTop >= 600 || firstAppearance >= 600) {
+    backToTop.style.display = 'inline-flex'
+  }
+  else if (document.body.scrollTop < 600 || firstAppearance < 600) {
+    backToTop.style.display = 'none'
+  }
+  if (maxAppearance - document.documentElement.scrollTop < 500) {
+    backToTop.style.bottom = `${document.documentElement.scrollTop - maxAppearance + 580}px`
+  }
+  else if (maxAppearance - document.documentElement.scrollTop >= 500) {
+    backToTop.style.bottom = '50px'
+  }
+}
+
+export default (function backToTop () {
+  window.addEventListener('scroll', showBackToTop)
+  
+})()
