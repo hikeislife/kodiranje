06237825c08290de57f4a01a372bae6c0ac4e8b5
hikeislife <hikeislife@users.noreply.github.com>
2020-03-02 22:33:36 +0100

    token test

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,13 +4,14 @@ const Admin = require('../db/models/admin')
 const auth = async (req, res, next) => {
   try {
     const cookie = await req.header('Cookie')
+    console.log(cookie)
     const cookies = cookie.split(';')
     let token = ''
     cookies.forEach(x => {
       if (x.split('=')[0] === 'token') token = x.split('=')[1]
     })
     console.log(token)
-
+    console.log('HELLO')
     const decoded = jwt.verify(token, process.env.JWT_P_KEY)
     const admin = await Admin.findOne({ 
       _id: decoded._id, 
