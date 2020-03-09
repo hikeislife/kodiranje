d24219d5b6173892d585961096fce7aac94898c7
hikeislife <hikeislife@users.noreply.github.com>
2020-03-09 02:08:47 +0100

    sw files

---
+++ src/js/serviceWorker/register.js
@@ -0,0 +1,17 @@
+export default (function registerSW () {
+  if ("serviceWorker" in navigator) {
+    if (navigator.serviceWorker.controller) {
+      console.log(`Dobro došli na sajt za učenje kodiranja i programiranja. Ovo je naša konzola :)`);
+    } else {
+      // Register the service worker
+      navigator.serviceWorker
+        .register("service-worker.js", {
+          //scope: "./kodiranje/"
+          scope: "./"
+        })
+        .then(function (reg) {
+          console.log(`Dobro došli na sajt za učenje kodiranja i programiranja. Ovo je naša konzola :)`);
+        });
+    }
+  }
+})()
