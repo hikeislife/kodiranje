e12e2a1345586f1fceb674746f756e1e1e52ffc7
hikeislife <hikeislife@users.noreply.github.com>
2020-03-10 20:25:24 +0100

    sitemap generator test

--- src/routers/articles.js
+++ src/routers/articles.js
@@ -144,8 +144,7 @@ findOrder = async (course) => {
 }
 
 generateSiteMap = async () => {
-  const header = `
-  <?xml version="1.0" encoding="UTF-8"?>
+  const header = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset
       xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">\n\r`
