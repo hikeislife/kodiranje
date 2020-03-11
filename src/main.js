032a87786747db5f33f933c2f8b51289bad07747
hikeislife <hikeislife@users.noreply.github.com>
2020-03-11 21:03:02 +0100

    front page icon update

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import createPageNav        from "./js/front/pageNav.js"
 import renderReferenceList  from "./js/front/renderReferenceList.js"
 import backToTop            from "./js/front/backToTop.js"
 import scrollPos            from "./js/utils/scrollPosIndicator.js"
-import register             from "./js/serviceWorker/register.js"
+//import register             from "./js/serviceWorker/register.js"
 
 window.onscroll = () => bindMenu()
 createPageNav()
